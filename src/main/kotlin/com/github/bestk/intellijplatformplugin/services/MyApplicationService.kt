1aed5019ca58d19388d33873b9d7ec45e8fcb76c
GitHub Action <action@github.com>
2022-09-22 07:13:30 +0000

    Template cleanup

--- src/main/kotlin/com/github/bestk/intellijplatformplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/bestk/intellijplatformplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.bestk.intellijplatformplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.bestk.intellijplatformplugin.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/bestk/intellijplatformplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/bestk/intellijplatformplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.bestk.intellijplatformplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.bestk.intellijplatformplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
